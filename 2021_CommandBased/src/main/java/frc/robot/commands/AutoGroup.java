0c5d8a7d5149be9a30ec47c19de00dd5a0328caf
Nathaniel <nphilbrick26@gmail.com>
2021-02-11 16:53:56 -0800

    Command groups added

---
+++ 2021_CommandBased/src/main/java/frc/robot/commands/AutoGroup.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.subsystems.Drive_s;
+import frc.robot.subsystems.Intake_s;
+
+public class AutoGroup extends SequentialCommandGroup{
+
+    public AutoGroup(Drive_s drive, Intake_s intake){
+
+    }
+}
