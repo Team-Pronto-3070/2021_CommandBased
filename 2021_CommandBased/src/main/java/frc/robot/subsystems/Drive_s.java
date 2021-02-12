9612a62c13b890a924d22a3cee66ec9e2f6317af
ProfessorX <35818467+beechnutkjr@users.noreply.github.com>
2021-02-11 16:00:52 -0800

    Insert packages

--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
@@ -1,3 +1,4 @@
+package frc.robot.subsystems;
 
 //First imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
@@ -1,3 +1,5 @@
+package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake_s extends SubsystemBase{
