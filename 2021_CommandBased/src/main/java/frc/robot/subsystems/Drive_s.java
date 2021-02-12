a5c23ec99449b9e6e269757444f698f479fcf6cf
ProfessorX <35818467+beechnutkjr@users.noreply.github.com>
2021-02-11 16:12:43 -0800

    Phoenix.json imports fixed

--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems;
 
 //First imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 //Motor control imports
 
 
@@ -10,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drive_s extends SubsystemBase{
+
 
     public Drive_s(){
 
--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Intake_s extends SubsystemBase{
 
     public Intake_s(){
-
+        
         
     }
 }
