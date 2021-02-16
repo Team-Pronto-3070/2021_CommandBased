92233e08ba7e44103f181229c286383b19249bdf
ProfessorX <35818467+beechnutkjr@users.noreply.github.com>
2021-02-16 14:35:31 -0800

    Update

--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Drive_s.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 //First imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
