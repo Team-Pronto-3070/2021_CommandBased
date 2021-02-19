58a3313dbac5042fd60febd63754ee7f0aa7d26b
Nathaniel <nphilbrick26@gmail.com>
2021-02-18 17:00:10 -0800

    OI and Robot conainers

--- 2021_CommandBased/src/main/java/frc/robot/RobotContainer.java
+++ 2021_CommandBased/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.AutoGroup;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drive_s;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Intake_s;
 import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +23,12 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drive_s drive = new Drive_s();
+  private final Intake_s intake = new Intake_s();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final AutoGroup autoCommand = new AutoGroup(drive,intake);
+
+  OI oi = new OI();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +42,18 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    oi.addButton("btn1", 1);
+    oi.addButton("btn3", 3);
+    oi.addButton("btn2", 2);
+    oi.addButton("btn4", 4);
+    oi.addButton("btn5", 5);
+
+
+    // Referencing the added buttons when pressed
+    
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +62,8 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    //return m_autoCommand;
+
+    return autoCommand;
   }
 }
