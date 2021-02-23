20ad55d914e8de6ae3c727549e8d609bf6d67dec
Abel Mitiku <abemit2000@gmail.com>
2021-02-22 18:19:07 -0800

    Created Talon, completed Intake class, deleted solenoid

--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
@@ -1,26 +1,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Intake_s extends SubsystemBase{
+    //Creates Talon
+    TalonSRX talIN;
+ 
 
     public Intake_s(){
-
-        
+        talIN = new TalonSRX(Constants.p_TAL_IN);
+       // talIN.setInverted(true);
     }
 
     /**
         Takes input to set speed of the intake motor
-     */
-    public void set(double input){
-
+     
+    @param velocity
+    */
+    public void set(double velocity){
+        talIN.set(ControlMode.PercentOutput, velocity);
     }
 
-    /**
-        Actuates intake solenoid
-     */
-    public void setSolenoid(boolean input){
-
-    }
+    
 }
