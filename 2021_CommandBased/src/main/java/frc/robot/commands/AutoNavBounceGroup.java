ebfecf6a22d4efdada3a0f8763c4249df51efce8
JulianRW1 <julianrwalston@gmail.com>
2021-03-09 13:55:50 -0800

    added a PathWeaver trajectory for the auto bounce path

--- 2021_CommandBased/src/main/java/frc/robot/commands/AutoNavBounceGroup.java
+++ 2021_CommandBased/src/main/java/frc/robot/commands/AutoNavBounceGroup.java
@@ -7,6 +7,17 @@ import frc.robot.subsystems.Drive_s;
 public class AutoNavBounceGroup extends SequentialCommandGroup {
     
     public AutoNavBounceGroup(Drive_s drive) {
-        
+                String trajectoryJSON = "paths/BouncePath.wpilib.json";
+        Trajectory trajectory = new Trajectory();
+        try {
+        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+        trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        } catch (IOException ex) {
+        DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+        }
+
+        addRequirements(drive);
+        addCommands(new XdriveTrajectoryCommand(trajectory, drive));
+
     }
 }
