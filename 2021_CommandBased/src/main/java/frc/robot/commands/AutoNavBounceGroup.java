7205a6ea673efc3e40efa73370f3b10d5b5a1c0a
JulianRW1 <julianrwalston@gmail.com>
2021-03-09 14:14:00 -0800

    added imports and corrected indentation

--- 2021_CommandBased/src/main/java/frc/robot/commands/AutoNavBounceGroup.java
+++ 2021_CommandBased/src/main/java/frc/robot/commands/AutoNavBounceGroup.java
@@ -1,5 +1,12 @@
 package frc.robot.commands;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.subsystems.Drive_s;
@@ -7,13 +14,13 @@ import frc.robot.subsystems.Drive_s;
 public class AutoNavBounceGroup extends SequentialCommandGroup {
     
     public AutoNavBounceGroup(Drive_s drive) {
-                String trajectoryJSON = "paths/BouncePath.wpilib.json";
+        String trajectoryJSON = "paths/BouncePath.wpilib.json";
         Trajectory trajectory = new Trajectory();
         try {
-        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-        trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
         } catch (IOException ex) {
-        DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
         }
 
         addRequirements(drive);
