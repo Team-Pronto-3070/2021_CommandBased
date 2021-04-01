3f7b12885b88fdcce2c49f523dc7b4b7f59549a9
Ben <66845228+benfeinberg@users.noreply.github.com>
2021-04-01 14:15:21 -0700

    reset the UKF when the pose estimator is reset

--- 2021_CommandBased/src/main/java/frc/robot/util/XdrivePoseEstimator.java
+++ 2021_CommandBased/src/main/java/frc/robot/util/XdrivePoseEstimator.java
@@ -150,6 +150,7 @@ public class XdrivePoseEstimator {
   public void resetPosition(Pose2d poseMeters, Rotation2d gyroAngle) {
     m_previousAngle = poseMeters.getRotation();
     m_gyroOffset = getEstimatedPosition().getRotation().minus(gyroAngle);
+    m_observer.reset();
     m_observer.setXhat(StateSpaceUtil.poseTo3dVector(poseMeters));
   }
 
