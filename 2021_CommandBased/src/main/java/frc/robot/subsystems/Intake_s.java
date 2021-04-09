210a26e318ee0474437be50bec9322d4f71440f7
Ben <66845228+benfeinberg@users.noreply.github.com>
2021-04-09 11:24:59 -0700

    clean up intake

--- 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
+++ 2021_CommandBased/src/main/java/frc/robot/subsystems/Intake_s.java
@@ -1,37 +1,19 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 import frc.robot.Constants;
 
 public class Intake_s extends SubsystemBase{
-    //Creates Talon
     TalonSRX talIN;
- 
 
-    public Intake_s(){
+    public Intake_s() {
         talIN = new TalonSRX(Constants.TAL_INTAKE_PORT);
-       // talIN.setInverted(true);
-
-       talIN.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
     }
 
-    @Override
-    public void periodic() {
-        SmartDashboard.putNumber("test encoder", talIN.getSelectedSensorPosition());
-    }
-
-    /**
-        Takes input to set speed of the intake motor
-     
-    @param velocity
-    */
     public void set(double velocity){
         talIN.set(ControlMode.PercentOutput, velocity);
     }
-
 }
