f8c4426d32370495ae351908591af95ea4a01dc3
Ben <66845228+benfeinberg@users.noreply.github.com>
2021-04-09 08:55:37 -0700

    clean up teleop command

--- 2021_CommandBased/src/main/java/frc/robot/commands/TeleopCommand.java
+++ 2021_CommandBased/src/main/java/frc/robot/commands/TeleopCommand.java
@@ -32,7 +32,6 @@ public class TeleopCommand extends CommandBase{
     addRequirements(_drive);
   }
 
-  //Overrides execute & periodically sends input to drivetrain
   @Override
   public void execute() {
     //Sets wheel speeds according to joystick values
@@ -40,85 +39,11 @@ public class TeleopCommand extends CommandBase{
     SmartDashboard.putNumber("teleop_y", _oi.getY());
     SmartDashboard.putNumber("teleop_theta", _oi.getTheta());
 
-//    _drive.setIndividual(
-//    _drive.setWheelSpeeds(
-//    closedLoop(
     _drive.setChassisSpeeds(
-//                    _drive.getKinematics().toWheelSpeeds(
-                              ChassisSpeeds.fromFieldRelativeSpeeds(
-                                  (Math.abs(_oi.getX()) < Constants.JOY_STICK_VX_DEADZONE) ? 0 : _oi.getX() * Constants.VX_COEFFICENT,
-                                  (Math.abs(_oi.getY()) < Constants.JOY_STICK_VY_DEADZONE) ? 0 : _oi.getY() * Constants.VY_COEFFICENT,
-                                  (Math.abs(_oi.getTheta()) < Constants.JOY_STICK_OMEGA_DEADZONE) ? 0 : _oi.getTheta() * Constants.OMEGA_COEFFICENT,
-                                  _drive.getTeleopRotation()))
-//                              .normalize(Constants.MAX_WHEEL_VELOCITY))
-                                                                ;
-  }
-
-  private void closedLoop(XdriveWheelSpeeds targetWheelSpeeds) {
-    var frontLeftSpeedSetpoint = 4 * targetWheelSpeeds.frontLeftMetersPerSecond;
-    var rearLeftSpeedSetpoint = 4 * targetWheelSpeeds.rearLeftMetersPerSecond;
-    var frontRightSpeedSetpoint = 4 * targetWheelSpeeds.frontRightMetersPerSecond;
-    var rearRightSpeedSetpoint = 4 * targetWheelSpeeds.rearRightMetersPerSecond;
-
-    SmartDashboard.putNumber("FL_SETPOINT", frontLeftSpeedSetpoint);
-    SmartDashboard.putNumber("FR_SETPOINT", frontRightSpeedSetpoint);
-    SmartDashboard.putNumber("BL_SETPOINT", rearLeftSpeedSetpoint);
-    SmartDashboard.putNumber("BR_SETPOINT", rearRightSpeedSetpoint);
-
-    double frontLeftOutput;
-    double rearLeftOutput;
-    double frontRightOutput;
-    double rearRightOutput;
-
-    var m_frontLeftController = Constants.FL_PID;
-    var m_frontRightController = Constants.FR_PID;
-    var m_rearLeftController = Constants.BL_PID;
-    var m_rearRightController = Constants.BR_PID;
-
-    var m_currentWheelSpeeds = _drive.getWheelSpeeds();
-
-    double curTime = timer.get();
-    double dt = curTime - prevTime;
-    prevTime = curTime;
-
-      final double frontLeftFeedforward =
-          Constants.FL_FF.calculate(
-              frontLeftSpeedSetpoint);
-
-      final double rearLeftFeedforward =
-          Constants.BL_FF.calculate(
-              rearLeftSpeedSetpoint);
-
-      final double frontRightFeedforward =
-          Constants.FR_FF.calculate(
-              frontRightSpeedSetpoint);
-
-      final double rearRightFeedforward =
-          Constants.BR_FF.calculate(
-              rearRightSpeedSetpoint);
-
-      frontLeftOutput =
-          frontLeftFeedforward
-              + m_frontLeftController.calculate(
-                  m_currentWheelSpeeds.frontLeftMetersPerSecond, frontLeftSpeedSetpoint);
-
-      rearLeftOutput =
-          rearLeftFeedforward
-              + m_rearLeftController.calculate(
-                  m_currentWheelSpeeds.rearLeftMetersPerSecond, rearLeftSpeedSetpoint);
-
-      frontRightOutput =
-          frontRightFeedforward
-              + m_frontRightController.calculate(
-                  m_currentWheelSpeeds.frontRightMetersPerSecond, frontRightSpeedSetpoint);
-
-      rearRightOutput =
-          rearRightFeedforward
-              + m_rearRightController.calculate(
-                  m_currentWheelSpeeds.rearRightMetersPerSecond, rearRightSpeedSetpoint);
-
-      _drive.setIndividual(
-          new XdriveWheelSpeeds(
-              frontLeftOutput, frontRightOutput, rearLeftOutput, rearRightOutput));
+            ChassisSpeeds.fromFieldRelativeSpeeds(
+                (Math.abs(_oi.getX()) < Constants.JOY_STICK_VX_DEADZONE) ? 0 : _oi.getX() * Constants.VX_COEFFICENT,
+                (Math.abs(_oi.getY()) < Constants.JOY_STICK_VY_DEADZONE) ? 0 : _oi.getY() * Constants.VY_COEFFICENT,
+                (Math.abs(_oi.getTheta()) < Constants.JOY_STICK_OMEGA_DEADZONE) ? 0 : _oi.getTheta() * Constants.OMEGA_COEFFICENT,
+                _drive.getTeleopRotation()));
   }
 }
